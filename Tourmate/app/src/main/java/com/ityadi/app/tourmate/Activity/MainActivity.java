0b0f85d1a65611ae730183368f3b37bb66333cfc
Mohammad Taslim <mtaslim@gmail.com>
2016-08-15 15:54:08 +0600

    Photo Library Created
    Photo Library Created

--- Tourmate/app/src/main/java/com/ityadi/app/tourmate/Activity/MainActivity.java
+++ Tourmate/app/src/main/java/com/ityadi/app/tourmate/Activity/MainActivity.java
@@ -1,15 +1,11 @@
 package com.ityadi.app.tourmate.Activity;
 
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -22,12 +18,11 @@ import com.ityadi.app.tourmate.Common.CurrentTime;
 import com.ityadi.app.tourmate.Common.Network;
 import com.ityadi.app.tourmate.Common.UserApi;
 import com.ityadi.app.tourmate.Common.UserApiWithoutPhoto;
+import com.ityadi.app.tourmate.PhotoLibrary.PhotoLibrary;
 import com.ityadi.app.tourmate.R;
 import com.ityadi.app.tourmate.Response.UserResponse;
 
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileDescriptor;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -43,31 +38,15 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     String name,username,password,email;
     EditText editTextName,editTextUsername,editTextPassword,editTextEmail;
-
     ImageView photoView;
-    //Button btnCapturePicture;
-
-    private static final String TAG = "CallCamera";
-    private static final String PHOTO_DIR = "Camera";
 
     String timeStamp,realPath="";
 
-    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
-    private static final int CAMERA_CAPTURE_VIDEO_REQUEST_CODE = 200;
-    public static final int MEDIA_TYPE_IMAGE = 1;
-    public static final int MEDIA_TYPE_VIDEO = 2;
-
-    private static final int RESULT_CAPTURE_IMAGE = 0;
-    private static int RESULT_LOAD_IMAGE = 1;
-
-
-    private static final String IMAGE_DIRECTORY_NAME = "My Camera";
-    public Uri fileUri; // file url to store image/video
+    public Uri fileUri;
 
     CurrentTime currentTime = new CurrentTime();
     Call<UserResponse> call;
-
-
+    PhotoLibrary photoLibrary = new PhotoLibrary();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -79,68 +58,49 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayShowCustomEnabled(true);
         getSupportActionBar().setCustomView(R.layout.custom_action_bar_layout);
+
         inputData();
+    }
+    public void inputData(){
+        editTextName = (EditText)findViewById(R.id.editTextName);
+        editTextUsername = (EditText)findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText)findViewById(R.id.editTextPassword);
+        editTextEmail = (EditText)findViewById(R.id.editTextEmail);
+        photoView = (ImageView) findViewById(R.id.photoView);
     }
 
     public void selectImage(View view) {
         Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        startActivityForResult(i, RESULT_LOAD_IMAGE);
+        startActivityForResult(i, photoLibrary.RESULT_LOAD_IMAGE);
     }
+
     public void captureImage(View view) {
         Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        fileUri = Uri.fromFile(getOutputPhotoFile());
+        fileUri = Uri.fromFile(photoLibrary.getOutputPhotoFile());
         i.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
-        startActivityForResult(i, RESULT_CAPTURE_IMAGE );
-    }
-    public File getOutputPhotoFile() {
-        File directory = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DCIM), PHOTO_DIR);
-        if (!directory.exists()) {
-            if (!directory.mkdirs()) {
-                Log.e(TAG, "Failed to create storage directory.");
-                return null;
-            }
-        }
-        return new File(directory.getPath() + File.separator + "IMG_" +currentTime.getTimestamp() + ".jpg");
+        startActivityForResult(i, photoLibrary.RESULT_CAPTURE_IMAGE );
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode == RESULT_OK){
-            if (requestCode == RESULT_LOAD_IMAGE) {
-
+            if (requestCode == photoLibrary.RESULT_LOAD_IMAGE) {
                 if(data != null) {
                     Uri selectedImage = data.getData();
-                    String[] filePathColumn = {MediaStore.Images.Media.DATA};
-
-                    Cursor cursor = getContentResolver().query(selectedImage,
-                            filePathColumn, null, null, null);
-                    cursor.moveToFirst();
-
-                    int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-                    String picturePath = cursor.getString(columnIndex);
-                    cursor.close();
-
-
-
                     Bitmap bmp = null;
                     try {
-                        bmp = getBitmapFromUri(selectedImage);
+                        bmp = photoLibrary.getBitmapFromUri(getBaseContext(),selectedImage);
                     } catch (IOException e) {
-                        // TODO Auto-generated catch block
                         e.printStackTrace();
                     }
                     photoView.setImageBitmap(bmp);
-
-                    Uri uri = getImageUri(getBaseContext(), bmp);
-                    realPath = getRealPathFromURI(uri);
-
-                    // Toast.makeText(getApplicationContext(), realPath, Toast.LENGTH_LONG).show();
+                    Uri uri = photoLibrary.getImageUri(getBaseContext(), bmp);
+                    realPath =  photoLibrary.getRealPathFromURI(getBaseContext(),uri);
                 } // if(data != null)
             }
-            else if (requestCode == RESULT_CAPTURE_IMAGE){
-                realPath = String.valueOf(getOutputPhotoFile());
+            else if (requestCode == photoLibrary.RESULT_CAPTURE_IMAGE){
+                realPath = String.valueOf(photoLibrary.getOutputPhotoFile());
                 File file = new File(realPath);
                 if(file.exists()){
                     Bitmap myBitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
@@ -148,37 +108,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
-    }
-    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
-        ParcelFileDescriptor parcelFileDescriptor =
-                getContentResolver().openFileDescriptor(uri, "r");
-        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
-        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
-        parcelFileDescriptor.close();
-        return image;
-    }
-
-    public Uri getImageUri(Context inContext, Bitmap inImage) {
-        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
-        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
-        return Uri.parse(path);
-    }
-
-    public String getRealPathFromURI(Uri uri) {
-        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
-        cursor.moveToFirst();
-        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
-        return cursor.getString(idx);
-    }
-
-
-    public void inputData(){
-        editTextName = (EditText)findViewById(R.id.editTextName);
-        editTextUsername = (EditText)findViewById(R.id.editTextUsername);
-        editTextPassword = (EditText)findViewById(R.id.editTextPassword);
-        editTextEmail = (EditText)findViewById(R.id.editTextEmail);
-        photoView = (ImageView) findViewById(R.id.photoView);
     }
 
     public void addUser(View view) {
@@ -239,6 +168,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+
+
 
 
 
